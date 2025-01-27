cd758420dc8a29dd5b13928e7850a6c5050f45a9
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-01-27 15:43:32 +0100

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -7,8 +7,8 @@ declare -A PROXMOX_USERS=(
     ["user3"]="pveadmin-user3@pve"
     ["user4"]="pveadmin-user4@pve"
 )
-PROXMOX_PASSWORD=""
-PVE_PASSWORD=""
+PROXMOX_PASSWORD="Azerty/123"
+PVE_PASSWORD="Azerty/123"
 
 # Variables fichiers ISO
 OPNSENSE_ISO=""
@@ -16,8 +16,8 @@ DEBIAN_ISO=""
 
 # Variables Stockage
 declare -A STORAGE_CONFIGS=(
-    ["local-lvm-template"]="/dev/sdb"
-    ["local-lvm-vm"]="/dev/sdc"
+    ["local-lvm-vm"]="/dev/sdb"
+    ["local-lvm-pve"]="/dev/sdc"
 )
 
 # Variables Disque pour les VMs
@@ -34,13 +34,13 @@ BOND_INTERFACES_2="eno2"         # Interfaces physiques 2 pour le bond0 (WAN)
 LAN_INTERFACE="eno3"              # Interface utilisée pour les VLANs 5, 10, 20
 ADMIN_INTERFACE="eno4"              # Interface dédiée à vmbr4
 
-WAN_IP="192.168.1.10"
-WAN_NETMASK="255.255.255.0"
-WAN_GATEWAY="192.168.1.1"
+WAN_IP="172.16.0.5"
+WAN_NETMASK="255.255.255.240"
+WAN_GATEWAY="172.16.0.14"
 WAN_HOSTNAME="pve-1"
-WAN_DOMAIN="localdomain"
+WAN_DOMAIN="sun.com"
 
-LAN_IP="192.168.100.10"
+LAN_IP="172.16.1.250"
 LAN_NETMASK="255.255.255.0"
 LAN_HOSTNAME="pve-1"
 LAN_DOMAIN="localdomain"
