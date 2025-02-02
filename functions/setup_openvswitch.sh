82d15f82bb501d8c3ec7d9a4d8aa99b32013e6ed
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-02 10:54:19 +0100

    Update setup_openvswitch.sh

--- functions/setup_openvswitch.sh
+++ functions/setup_openvswitch.sh
@@ -149,12 +149,15 @@ $LAN_IP    $LAN_HOSTNAME.$LAN_DOMAIN $LAN_HOSTNAME
 EOF
 
     # Ajout des interfaces aux bridges OVS
-    ovs-vsctl add-port $WAN_BRIDGE $BOND_INTERFACE_1
-    ovs-vsctl add-port $LAN_BRIDGE $ADMIN_INTERFACE
-    ovs-vsctl add-port $VLAN_BRIDGE $LAN_INTERFACE
-    ovs-vsctl add-port $VLAN_BRIDGE vlan5
-    ovs-vsctl add-port $VLAN_BRIDGE vlan10
-    ovs-vsctl add-port $VLAN_BRIDGE vlan20
+    ovs-vsctl add-port vmbr4 $ADMIN_INTERFACE
+    ovs-vsctl add-port vmbr3 $LAN_INTERFACE
+    ovs-vsctl add-port vmbr3 vlan5
+    ovs-vsctl add-port vmbr3 vlan10
+    ovs-vsctl add-port vmbr3 vlan20
+    ovs-vsctl add-port vmbr2 vlan30
+    ovs-vsctl add-port vmbr2 vlan40
+    ovs-vsctl add-port vmbr1 vlan50
+    ovs-vsctl add-port vmbr1 vlan60
 
     echo "Redémarrage des services réseau..."
     systemctl restart networking
