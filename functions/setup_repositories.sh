a09b7cffa090c2fecb2fab14f389d4513f6f435c
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-02 10:17:06 +0100

    Update setup_repositories.sh

--- functions/setup_repositories.sh
+++ functions/setup_repositories.sh
@@ -2,21 +2,19 @@
 
 # Désactivation des dépôts d'entreprise et ajout des dépôts communautaires
 setup_repositories() {
-
     echo "Création des groupes de ressources..."
     for pool in pare-feu zone-relais zone-exposee service-interne template testing; do
         pveum pool add $pool || { echo "Erreur de création du groupe $pool"; exit 1; }
     done
     
     echo "Groupes de ressources créés avec succès."
+    
     echo "Suppression des fichiers de dépôt d'entreprise Proxmox..."
     rm -f /etc/apt/sources.list.d/pve-enterprise.list
-    rm -f /etc/apt/sources.list.d/ceph-quincy.list
-
+    
     echo "Nettoyage des références problématiques..."
     sed -i '/enterprise.proxmox.com/d' /etc/apt/sources.list
-    sed -i '/ceph-quincy/d' /etc/apt/sources.list.d/*.list
-
+    
     echo "Activation des dépôts non commerciaux de Proxmox..."
     echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
 
