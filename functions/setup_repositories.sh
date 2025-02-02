ca3981bc7676ebca668e5e01954fe3fb451595ac
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-02 12:51:13 +0100

    Update setup_repositories.sh

--- functions/setup_repositories.sh
+++ functions/setup_repositories.sh
@@ -9,26 +9,19 @@ setup_repositories() {
     
     echo "Groupes de ressources créés avec succès."
     
-    # Suppression des fichiers de dépôt d'entreprise Proxmox
-    echo "Suppression des fichiers de dépôt d'entreprise Proxmox..."
+    # Suppression des fichiers de dépôt d'entreprise Proxmox et ceph-quincy
+    echo "Suppression des fichiers de dépôt d'entreprise Proxmox et de ceph-quincy..."
     rm -f /etc/apt/sources.list.d/pve-enterprise.list
-    # Ne pas supprimer ceph-quincy.list si vous souhaitez conserver Ceph
-    # rm -f /etc/apt/sources.list.d/ceph-quincy.list (ne pas supprimer)
+    rm -f /etc/apt/sources.list.d/ceph-quincy.list (ne pas supprimer)
 
     # Nettoyage des références problématiques
     echo "Nettoyage des références problématiques..."
     sed -i '/enterprise.proxmox.com/d' /etc/apt/sources.list
-    
-    # Supprimer les références Ceph
     sed -i '/ceph-quincy/d' /etc/apt/sources.list.d/*.list (ne pas supprimer)
 
     # Activation des dépôts non commerciaux de Proxmox
     echo "Activation des dépôts non commerciaux de Proxmox..."
     echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
-
-    # Ajouter les dépôts de sécurité Debian si nécessaire
-    #echo "Ajout du dépôt de sécurité Debian..."
-    #echo "deb http://security.debian.org/debian-security bookworm-security main" | tee -a /etc/apt/sources.list
 
     # Ajouter le dépôt HashiCorp pour Terraform
     echo "Ajout du dépôt HashiCorp..."
