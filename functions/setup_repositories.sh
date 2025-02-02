9716ae4a0231b8a8c24bbaba490082cfb3d67331
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-02 13:28:57 +0100

    Update setup_repositories.sh

--- functions/setup_repositories.sh
+++ functions/setup_repositories.sh
@@ -13,16 +13,16 @@ setup_repositories() {
     echo "Suppression des fichiers de dépôt d'entreprise Proxmox et de ceph-quincy..."
     rm -f /etc/apt/sources.list.d/pve-enterprise.list
     rm -f /etc/apt/sources.list.d/ceph-quincy.list (ne pas supprimer)
-
+    
     # Nettoyage des références problématiques
     echo "Nettoyage des références problématiques..."
     sed -i '/enterprise.proxmox.com/d' /etc/apt/sources.list
-    sed -i '/ceph-quincy/d' /etc/apt/sources.list.d/*.list (ne pas supprimer)
-
+    sed -i '/ceph-quincy/d' /etc/apt/sources.list.d/*.list
+    
     # Activation des dépôts non commerciaux de Proxmox
     echo "Activation des dépôts non commerciaux de Proxmox..."
     echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
-
+    
     # Ajouter le dépôt HashiCorp pour Terraform
     echo "Ajout du dépôt HashiCorp..."
     wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
