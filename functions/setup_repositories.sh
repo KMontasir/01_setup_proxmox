47f97962af86bf2eda450897ffe4046a07e93ddc
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-02 13:58:14 +0100

    Update setup_repositories.sh

--- functions/setup_repositories.sh
+++ functions/setup_repositories.sh
@@ -34,6 +34,7 @@ setup_repositories() {
     echo "Ajout du dépôt HashiCorp..."
     wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
     gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+    DISTRO_NAME=$(grep VERSION_CODENAME /etc/os-release | cut -d '=' -f2)
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $DISTRO_NAME main" | tee /etc/apt/sources.list.d/hashicorp.list
     apt update -y
 }
