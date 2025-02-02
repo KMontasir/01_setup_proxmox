d852769a3a8e602d7719efe1f14e583b64ddae8c
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-02 09:30:12 +0100

    Update setup_users.sh

--- functions/setup_users.sh
+++ functions/setup_users.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-# Fonction pour configurer les utilisateurs
+# Fonction pour configurer les utilisateurs de Proxmox
 setup_users() {
     echo "Création et configuration des utilisateurs..."
     for user in "${!PROXMOX_USERS[@]}"; do
         proxmox_user=${PROXMOX_USERS[$user]}
-        useradd -m -s /bin/bash "$user" && echo "$user:$PROXMOX_PASSWORD" | chpasswd
+        
+        # Création de l'utilisateur Linux
+        useradd -m -s /bin/bash "$user"
         usermod -aG sudo "$user"
-        mkdir /home/$user/.ssh/
+        mkdir -p /home/$user/.ssh/
         touch /home/$user/.ssh/authorized_keys
         chown -R $user:$user /home/$user/
+        chmod 700 /home/$user/.ssh
+        chmod 600 /home/$user/.ssh/authorized_keys
 
-        pveum user add "$proxmox_user" --password $PVE_PASSWORD
+        # Création de l'utilisateur Proxmox sans mot de passe
+        pveum user add "$proxmox_user" --comment "$user for automation"
         pveum acl modify / --users "$proxmox_user" --roles Administrator
-        pveum user token add "$proxmox_user" mytoken --privsep 1
-        echo "Récupérez les informations du token pour $proxmox_user !"
+
+        # Création du token API avec un nom unique
+        TOKEN_NAME="${user}_token"
+        pveum user token add "$proxmox_user" "$TOKEN_NAME" --privsep 1
+        
+        echo "Récupérez les informations du token pour $proxmox_user :"
+        echo "Utilisateur : $proxmox_user"
+        echo "Token Name : $TOKEN_NAME"
+        echo "Utilisez 'user@pam!$TOKEN_NAME=<TOKEN_VALUE>' pour l'authentification API."
         echo "Appuyez sur Entrer pour continuer..."
         read
     done
