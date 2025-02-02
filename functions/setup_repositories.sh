276fc46ba69623ac75fcf7b96c562aa03685303b
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-02 12:53:41 +0100

    Update setup_repositories.sh

--- functions/setup_repositories.sh
+++ functions/setup_repositories.sh
@@ -25,6 +25,8 @@ setup_repositories() {
 
     # Ajouter le dépôt HashiCorp pour Terraform
     echo "Ajout du dépôt HashiCorp..."
+    wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+    gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
     
     # Mise à jour des dépôts
