97ddb829dce1082e49ca7c2cab746ea8be1fe1a1
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-02-03 16:07:39 +0100

    Update setup_openvswitch.sh

--- functions/setup_openvswitch.sh
+++ functions/setup_openvswitch.sh
@@ -37,7 +37,6 @@ iface bond0 inet manual
 
 # Bridge WAN (vmbr0)
 auto vmbr0
-allow-ovs vmbr0
 iface vmbr0 inet static
     address $WAN_IP
     netmask $WAN_NETMASK
@@ -47,7 +46,6 @@ iface vmbr0 inet static
 
 # Bridge pour eno4 (vmbr4)
 auto vmbr4
-allow-ovs vmbr4
 iface vmbr4 inet static
     address $LAN_IP
     netmask $LAN_NETMASK
@@ -56,83 +54,74 @@ iface vmbr4 inet static
 
 # Bridge pour VLANs 5, 10 et 20 sur eno3 (vmbr3)
 auto vmbr3
-allow-ovs vmbr3
 iface vmbr3 inet manual
     ovs_type OVSBridge
-    ovs_ports $LAN_INTERFACE
+    ovs_ports $LAN_INTERFACE vlan5 vlan10 vlan20
 
 # VLAN 5 sur vmbr3
 auto vlan5
-allow-vmbr3 vlan5
 iface vlan5 inet manual
     ovs_type OVSIntPort
-    ovs_tag 5
     ovs_bridge vmbr3
+    ovs_options tag=5
 
 # VLAN 10 sur vmbr3
 auto vlan10
-allow-vmbr3 vlan10
 iface vlan10 inet manual
     ovs_type OVSIntPort
-    ovs_tag 10
     ovs_bridge vmbr3
+    ovs_options tag=10
 
 # VLAN 20 sur vmbr3
 auto vlan20
-allow-vmbr3 vlan20
 iface vlan20 inet manual
     ovs_type OVSIntPort
-    ovs_tag 20
     ovs_bridge vmbr3
+    ovs_options tag=20
 
 # Bridge pour VLANs 30 et 40 (vmbr2)
 auto vmbr2
-allow-ovs vmbr2
 iface vmbr2 inet manual
     ovs_type OVSBridge
+    ovs_ports vlan30 vlan40
 
 # VLAN 30 sur vmbr2
 auto vlan30
-allow-vmbr2 vlan30
 iface vlan30 inet manual
     ovs_type OVSIntPort
-    ovs_tag 30
     ovs_bridge vmbr2
+    ovs_options tag=30
 
 # VLAN 40 sur vmbr2
 auto vlan40
-allow-vmbr2 vlan40
 iface vlan40 inet manual
     ovs_type OVSIntPort
-    ovs_tag 40
     ovs_bridge vmbr2
+    ovs_options tag=40
 
 # Bridge pour VLANs 50 et 60 (vmbr1)
 auto vmbr1
-allow-ovs vmbr1
 iface vmbr1 inet manual
     ovs_type OVSBridge
+    ovs_ports vlan50 vlan60
 
 # VLAN 50 sur vmbr1
 auto vlan50
-allow-vmbr1 vlan50
 iface vlan50 inet manual
     ovs_type OVSIntPort
-    ovs_tag 50
     ovs_bridge vmbr1
+    ovs_options tag=50
 
 # VLAN 60 sur vmbr1
 auto vlan60
-allow-vmbr1 vlan60
 iface vlan60 inet manual
     ovs_type OVSIntPort
-    ovs_tag 60
     ovs_bridge vmbr1
+    ovs_options tag=60
 EOF
 
     echo "Mise à jour du fichier /etc/hosts..."
-    # Mise à jour du fichier /etc/hosts
-    echo "Sauvegarde de /etc/hosts..."
+    # Sauvegarde du fichier /etc/hosts
     cp $HOSTS_FILE ${HOSTS_FILE}.backup.$(date +%Y%m%d%H%M%S)
 
     # Ajout des entrées WAN et LAN
@@ -150,10 +139,16 @@ EOF
     systemctl restart networking
 
     echo "Ajout des interfaces aux bridges OVS"
-    # Ajout des interfaces aux bridges OVS pour éviter les erreurs
     ovs-vsctl add-port vmbr0 bond0
     ovs-vsctl add-port vmbr4 $ADMIN_INTERFACE
     ovs-vsctl add-port vmbr3 $LAN_INTERFACE
+    ovs-vsctl add-port vmbr3 vlan5
+    ovs-vsctl add-port vmbr3 vlan10
+    ovs-vsctl add-port vmbr3 vlan20
+    ovs-vsctl add-port vmbr2 vlan30
+    ovs-vsctl add-port vmbr2 vlan40
+    ovs-vsctl add-port vmbr1 vlan50
+    ovs-vsctl add-port vmbr1 vlan60
 
     echo "Redémarrage des services réseau... 2/2"
     systemctl restart openvswitch-switch
@@ -162,3 +157,5 @@ EOF
     echo "Configuration Open vSwitch terminée."
     echo "Vérifiez la configuration avec 'ovs-vsctl show' et 'ip a'."
 }
+
+setup_openvswitch
